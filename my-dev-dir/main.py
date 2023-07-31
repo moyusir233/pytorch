0b69b7ed092ad4f6649aad7282e393cc7978870b
moyu <279972161@qq.com>
2023-07-31 04:05:01 +0000

    feat: add some config files of src reading

---
+++ my-dev-dir/main.py
@@ -0,0 +1,8 @@
+import torch
+import torch.distributed as dist
+print("{}".format(torch.cuda.is_available()))
+
+dist.init_process_group("nccl","tcp://localhost:80",world_size=1,rank=0)
+print(
+dist.is_available()
+)
