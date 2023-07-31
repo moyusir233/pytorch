0c960514d5286a3caf741470cc97bdc782f091a4
moyu <279972161@qq.com>
2023-07-31 16:47:32 +0800

    update

--- my-dev-dir/main.py
+++ my-dev-dir/main.py
@@ -1,5 +1,7 @@
 import torch
 import torch.distributed as dist
+import os
+
 print("{}".format(torch.cuda.is_available()))
 
 dist.init_process_group("nccl","tcp://localhost:80",world_size=1,rank=0)
